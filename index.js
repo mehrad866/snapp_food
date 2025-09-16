37ac9244a1bb68139cb617e7146bbdf6f174d5b8
meto13902 <meto13902@gmail.com>
2025-09-16 10:07:11 -0700

    add json to project

---
+++ index.js
@@ -0,0 +1,6 @@
+let pasta = document.getElementById("khred-pasta")
+let petzza = document.getElementById("khred-petzza")
+let brger = document.getElementById("khred-brger")
+let salad = document.getElementById("khred-salad")
+
+document.addEventListener
