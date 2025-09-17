bfb25919905890d3fae2266705f84bdcc4729324
meto13902 <meto13902@gmail.com>
2025-09-17 02:08:30 -0700

    add some js and json and html to project

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ let pasta = 0
 let pizza = 0
 let brger = 0
 let salad = 0
+let price
 let data
 let fetchedData = []
+let btn_pasta = document.getElementById("btn_pasta")
+let btn_pizza = document.getElementById("btn_pizza")
+let btn_brger = document.getElementById("btn_brger")
+let btn_salad = document.getElementById("btn_salad")
+let p_sbad = document.getElementById("sbad")
 async function fetchdata(){
     try{
         let responce = await fetch ("index.json",{method: "get"})
@@ -12,8 +18,6 @@ async function fetchdata(){
         data.product.name.forEach((item) => {
             fetchedData.push(item)
         })
-        console.log(fetchedData);
-        
     }catch(error){
         console.log(error);
     }
@@ -22,3 +26,31 @@ async function fetchdata(){
         
     }
 } 
+
+btn_brger.addEventListener("click" , function (){
+    brger++
+    render()
+})
+btn_pasta.addEventListener("click" , function (){
+    pasta++
+    render()
+})
+btn_pizza.addEventListener("click" , function (){
+    pizza++
+    render()
+})
+btn_salad.addEventListener("click" , function (){
+    salad++
+    render()
+})
+function render(){
+    price = fetchedData[0].cost*pasta+ fetchedData[1].cost*brger+ fetchedData[2].cost*pizza+ fetchedData[3].cost*salad
+    p_sbad.innerHTML=`
+    
+    <p>تعداد برگر ${brger}</p>
+    <p>ـعداد پیتزا ${pizza}</p>
+    <p> تعداد سالاد${salad}</p>
+    <p>تعداد پاستا ${pasta}</p>
+    <p>مجموع قیمت ${price}</p>
+    `
+}
